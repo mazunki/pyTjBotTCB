8293bb88200e6f2b7bb03edff789124f4c79cdea
mazunki <rolferen@gmail.com>
2019-03-09 23:34:05 +0100

    clearer syntax

--- tts.py
+++ tts.py
@@ -7,11 +7,12 @@ stt_creds = credentials["text_to_speech"]
 tts_auth = tts(iam_apikey=stt_creds["api_key"], url=stt_creds["url"])
 
 def watson_play(play_text):
+    watson_stream = tts_auth.synthesize(play_text, 
+                                        "audio/wav"
+                                        )
+    synthetic_voice = watson_stream.get_result().content
+    
     with open("output_file.wav", "wb") as audio_file:
-        audio_file.write(
-            tts_auth.synthesize(
-                play_text, "audio/wav"
-            ).get_result().content
-        )
+        audio_file.write(synthetic_voice)
     
     play_audio("output_file.wav")
