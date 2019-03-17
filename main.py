286b244c28c6db74f40e462b1e8ed468d0c06a37
mazunki <rolferen@gmail.com>
2019-03-17 13:37:15 +0100

    Added error handling and safe closing from watson web_socket

--- main.py
+++ main.py
@@ -10,22 +10,26 @@ if __name__ == "__main__":
     
     th_audio_out = Thread(target=audioout.init_audioout)
     th_audio_in =  Thread(target=audioin.init_audioin)
-    th_watson_stt_socket = Thread(target=stt.stt_ws)
+    th_watson_stt_socket = Thread(target=stt.stt_ws_listener)
     
     th_audio_out.start()
     time.sleep(1)
     th_audio_in.start()
-    time.sleep(1)
+    time.sleep(5)
     th_watson_stt_socket.start()
     time.sleep(5)
 
 
     try:
         while True:
-            tts.watson_play("hello how are you doing")
-            pass
+            # tts.watson_play("hello how are you doing")
+            if stt.error_call == True:
+                audioin.stopped = True
+                audioout.stopped = True
+            else:
+                pass
     except KeyboardInterrupt:
-        th_audio_out.not_stopped = False
-        th_audio_in.not_stopped = False
+        audioout.stopped = True
+        audioin.stopped = True
     
     #stt.watson_stt()
--- stt.py
+++ stt.py
@@ -4,8 +4,10 @@ from watson_developer_cloud.websocket import RecognizeCallback, AudioSource
 from threading import Thread
 
 from creds import credentials
-import text_handler
+#import text_handler
 import audioin
+
+error_call = False
 
 class MyRecognizeCallback(RecognizeCallback):
     """
@@ -13,59 +15,69 @@ class MyRecognizeCallback(RecognizeCallback):
     """
     def __init__(self):
         RecognizeCallback.__init__(self)
+        print("Callback set.")
 
     def on_transcription(self, transcript):
-        print("transc") # We're using on_data() to handle everything. This function dismisses valuable information, 
-              # and only shows the raw text. Useful for printing output, maybe.
-
+        print("Transcription done") 
+    
     def on_connected(self):
         print('Connection to IBM Watson was successful')
 
     def on_error(self, error):
         print('Error received: {}'.format(error))
+        global error_call
+        error_call = True
 
     def on_inactivity_timeout(self, error):
         print('Inactivity timeout: {}'.format(error))
 
     def on_listening(self):
-        print('Watson is now listening')
+        print('Watson is listening...')
 
     def on_hypothesis(self, hypothesis):
-        print("hypo")
-        # Interim results before on_transcription. Same deal.
+        print("Hypotheses received")
+        # Interim results before on_transcription. Not received if interim_results=False
 
     def on_data(self, data):
-        print("handling")
-        text_handler.parse_text(data)
+        print("Data received:")
+        #text_handler.parse_text(data)
+        print(data)
 
     def on_close(self): 
         print("Connection closed")
-    
+            
     
 stt_creds = credentials["speech_to_text"]
 
 stt_auth = stt(iam_apikey=stt_creds["api_key"], url=stt_creds["url"])
+mycallback = MyRecognizeCallback()
 
-def stt_ws(*args):
+def stt_ws_listener():
     print("Preparing Watson websocket...")
-    mycallback = MyRecognizeCallback()
-    print("Callback set")
 
     watson_audio_stack = AudioSource(audioin.stack, is_recording=True, is_buffer=True)
     print("Watson Audio stack created.")
     
     stt_auth.recognize_using_websocket( 
         audio=watson_audio_stack, 
-        content_type="audio/l16;rate=88200", # rate = audioin.py's RATE*CHANNELS 
+        content_type="audio/flac", # rate = audioin.py's RATE*CHANNELS 
         recognize_callback=mycallback,
         interim_results=True, # Print all attempts, including not final responses.
     )
 
-    print("Socket set up. Listening.")
 
-    while True:
-        pass
-    
-        
+def stt_file(name):
+    print("Opening file...")
+    with open(name, "rb") as audio_file:
+        watson_audio_stack = AudioSource(audio_file)
+
+        print("Sending file to Watson...")
+        stt_auth.recognize_using_websocket( 
+            audio=watson_audio_stack, 
+            content_type="audio/wav", # rate = audioin.py's RATE*CHANNELS 
+            recognize_callback=mycallback,
+            interim_results=True, # Print all attempts, including not final responses.
+        )
+
 if __name__ == '__main__':
-    watson_stt()
+    stt_file("testing.wav")
