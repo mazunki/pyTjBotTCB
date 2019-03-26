e34e88df1380f7920ac87d8456a30bef2254ee3e
mazunki <rolferen@gmail.com>
2019-03-26 23:42:11 +0100

    pushing to test

--- chat_client.py
+++ chat_client.py
@@ -4,8 +4,10 @@ import time
 import threading
 from select import select
 
-TARGET_IP = "localhost"
+TARGET_IP = "srv.circuitbreakers.tech"
 TARGET_PORT = 13131
+
+MY_NAME = None
 
 s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 s.connect((TARGET_IP, TARGET_PORT))
@@ -28,19 +30,60 @@ def sender():
 
 def listener():
 	try:
+		global MY_NAME
+		if MY_NAME == None:
+			incoming = s.recv(1024).decode("ascii")
+			if "set_name " in incoming:
+				MY_NAME = incoming.replace("set_name ", "")
+				print("my name is", MY_NAME)
+			else:
+				print("<== Warning. Client has no name. ==>")
+		else:
+			print(MY_NAME)
+
 		while mouth.isAlive():
 			s.setblocking(0)
-
 			ready = select([s], [], [], 5) # seconds
 			if ready[0]:
 				incoming = s.recv(1024)
 				print(incoming.decode("ascii"), end="")
 				print("\n> ", sep="", end="")
+
+				network_parser(incoming.decode("ascii"))
+
 			#time.sleep(2)
 
 	except Exception as e:
 		s.close()
 		print(e)
+
+def network_parser(text_to_parse):
+	
+	start_of_message = text_to_parse.find(":") # "('127.12.34.56', 34278): hello world!!
+
+	try:
+		assert start_of_message != -1  # Invalid
+
+		source = text_to_parse[:start_of_message]  # "('127.12.34.56', 34278)"
+		assert source[0] == "(" and source[-1] == ")"
+
+		source = source[1:-1]  # "'127.12.34.56', 34278"
+
+		conn = source.split(", ")  # ["'127.12.34.56'", "34278"]
+		conn_ip = conn[0].replace("'","").replace('"','')  # "127.12.34.56"
+		conn_port = int(conn[1])  # 24278
+		conn = (conn_ip, conn_port) # cleaned up
+
+		message = text_to_parse[start_of_message+2:]  # ignoring the space
+
+		tuplified_msg = tuple(message.split(" "))  # oh god this can't be a good idea
+
+		if tuplified_msg[0] == MY_NAME:
+			pass
+
+	except AssertionError:
+		return
+
 
 mouth = threading.Thread(target=sender)
 ears = threading.Thread(target=listener)
