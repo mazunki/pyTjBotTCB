22b3363ca47fb9b8a6427dd3e8ed7fc3dd471365
mazunki <rolferen@gmail.com>
2019-03-31 15:18:04 +0200

    change order of import

--- neopixels/led_controller.py
+++ neopixels/led_controller.py
@@ -20,11 +20,11 @@ def add_to_led(item):
 		led_stack.put(item)
 
 def init_led():
+	led_board = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
 	import neopixels.matrix as matrix
 	
 	global led_board
 	add_support()
-	led_board = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
 	while True:
 		if led_stack.empty() == False:
