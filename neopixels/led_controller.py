1a65d0bb67d2c2846b47e8316de178705aab84e9
mazunki <rolferen@gmail.com>
2019-03-31 19:56:56 +0200

    indent and tabs to spaces fix

--- neopixels/led_controller.py
+++ neopixels/led_controller.py
@@ -16,33 +16,33 @@ led_stack = Queue(maxsize=3)
 supported = dict()
 
 def add_to_led(item):
-	if item in supported.keys():
-		led_stack.put(item)
+    if item in supported.keys():
+        led_stack.put(item)
 
 def init_led():
-	global led_board
-	led_board = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
-	import neopixels.matrix as matrix
-	
-	add_support()
+    global led_board
+    led_board = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
+    import neopixels.matrix as matrix
+    
+    add_support()
 
-	while True:
-	    print("new check")	
-            if led_stack.empty() == False:
-		matrix.worm_alive = False
-		desired_function = led_stack.get()
-		supported[desired_function]
-	    else:
-		worm_alive = True
-		matrix.worm()
+    while True:
+        print("new check")    
+        if led_stack.empty() == False:
+	        matrix.worm_alive = False
+	        desired_function = led_stack.get()
+        	supported[desired_function]
+        else:
+    	    worm_alive = True
+ 	       matrix.worm()
 
 def add_support():
-	import neopixels.matrix as matrix
-	global supported
-	supported = {
-		"police": matrix.police,
-		"rainbow": matrix.rainbow
-	}
+    import neopixels.matrix as matrix
+    global supported
+    supported = {
+        "police": matrix.police,
+        "rainbow": matrix.rainbow
+    }
 
 add_to_led("police")
 init_led()
