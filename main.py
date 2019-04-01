9d06c21ee4a647dae87865bb5ef85a2609b1a0f8
mazunki <rolferen@gmail.com>
2019-04-01 18:45:28 +0200

    DON'T CALL YOUR FILES TEMPFILE
    been 4 hours debbugging lol

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import watson.tts as tts
 import watson.stt as stt
 import audio.audioout as audioout
 import audio.audioin as audioin
-#import neopixels.led_controller as led_controller
+import neopixels.led_controller as led_controller
 
 from threading import Thread
 import time
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     th_audio_out = Thread(target=audioout.init_audioout, name="maznoski")
     th_audio_in =  Thread(target=audioin.init_audioin, name="MiceLitoris")
     th_watson_stt_socket = Thread(target=stt.stt_ws_listener, name="mr_watson")
-    #th_led = Thread(target=led_controller.init_led, name="god")
+    th_led = Thread(target=led_controller.init_led, name="god")
     
     th_audio_out.start()
     time.sleep(1)
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     th_watson_stt_socket.start()
     time.sleep(5)
     th_audio_out.start()
-    #th_led.start()
+    th_led.start()
 
 
     try:
