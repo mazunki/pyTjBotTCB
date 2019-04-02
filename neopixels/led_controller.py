ca4e72cdd9ddc779da669f4da33efb33018ff185
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2019-04-02 15:12:55 +0200

    Update led_controller.py

--- neopixels/led_controller.py
+++ neopixels/led_controller.py
@@ -2,18 +2,14 @@ import board
 import neopixel
 from queue import *
 
-
-
 WIDTH = 8
 HEIGHT = 1
 NUM_PIXELS = WIDTH*HEIGHT
 
 ORDER = neopixel.GRB
-
 PIXEL_PIN = board.D18
 
 led_board = 0
-
 
 led_stack = Queue(maxsize=3)
 supported = dict()
@@ -25,6 +21,7 @@ def add_to_led(item):
 def init_led():
     global led_board
     led_board = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.2, auto_write=False, pixel_order=ORDER)
+    print(led_board)
     import neopixels.matrix as matrix
     
     add_support()
