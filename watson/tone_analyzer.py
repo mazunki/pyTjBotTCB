ed1e789559618c8b4fe71dc0569711f8ad8be149
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2019-04-02 12:34:29 +0200

    Update tone_analyzer.py

--- watson/tone_analyzer.py
+++ watson/tone_analyzer.py
@@ -18,7 +18,9 @@ tone_anal = toneanal(
 sample_text = "I am really pissed at you now. You could have done better. Do you really think you could get away with what you have done? You ignorant piece of shit."
 
 def analyse_text(text=sample_text, num_tones=1): # 7 is the currently highest number of tones
-
+    """
+    Sends a string to Watson and parses an output from the json returned
+    """        
     print("analysing tone...", text)
     data_to_analyze = {
             "text": text
