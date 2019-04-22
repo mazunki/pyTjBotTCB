ec683f23e93e8ad0f63ffcf9e5d2f55bd8bff59d
mazunki <rolferen@gmail.com>
2019-04-22 16:09:18 +0200

    started adding encryption

--- server_communication/chat_client.py
+++ server_communication/chat_client.py
@@ -19,120 +19,120 @@ s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 s.connect((TARGET_IP, TARGET_PORT))
 
 def request_name(name):
-	name_command = "set name " + name
-	s.sendall(name_command.encode("ascii"))
+    name_command = "set name " + name
+    s.sendall(name_command.encode("ascii"))
 
 
 def sender():
-	try:
-		message = None
-		while True:
-			message = input("> ")
-			#time.sleep(0.2)
-			if message in ["end", "bye", "q"]:
-				break
+    try:
+        message = None
+        while True:
+            message = input("> ")
+            #time.sleep(0.2)
+            if message in ["end", "bye", "q"]:
+                break
 
-			elif "my name is" in message:
-				request_name(message[message.find("my name is")+len("my name is")+1:])
+            elif "my name is" in message:
+                request_name(message[message.find("my name is")+len("my name is")+1:])
 
-			else:
-				s.sendall(message.encode("ascii"))
+            else:
+                s.sendall(message.encode("ascii"))
 
-	except Exception as e:
-		print(e)
-		s.close()
-		return
-	finally:
-		s.close()
-		return
+    except Exception as e:
+        print(e)
+        s.close()
+        return
+    finally:
+        s.close()
+        return
 
 def listener():
-	try:
-		global MY_NAME
-		if MY_NAME == None:
-			incoming = s.recv(1024).decode("ascii")
-			if "set_name " in incoming:
-				MY_NAME = incoming.replace("set_name ", "")
-				print("my name is", MY_NAME)
-			else:
-				print("<== Warning. Client has no name. ==>")
-		else:
-			print(MY_NAME)
+    try:
+        global MY_NAME
+        if MY_NAME == None:
+            incoming = s.recv(1024).decode("ascii")
+            if "set_name " in incoming:
+                MY_NAME = incoming.replace("set_name ", "")
+                print("my name is", MY_NAME)
+            else:
+                print("<== Warning. Client has no name. ==>")
+        else:
+            print(MY_NAME)
 
-		while mouth.isAlive():
-			s.setblocking(0)
-			ready = select([s], [], [], 5) # seconds
-			if ready[0]:
-				incoming = s.recv(1024).decode("ascii")
-				if incoming != "":
-					print(incoming, end="")
-					print("\n> ", sep="", end="")
+        while mouth.isAlive():
+            s.setblocking(0)
+            ready = select([s], [], [], 5) # seconds
+            if ready[0]:
+                incoming = s.recv(1024).decode("ascii")
+                if incoming != "":
+                    print(incoming, end="")
+                    print("\n> ", sep="", end="")
 
-				if "set_name " in incoming:
-					MY_NAME = incoming.replace("set_name ", "")
-					print("my name is", MY_NAME)
+                    if "set_name " in incoming:
+                        MY_NAME = incoming.replace("set_name ", "")
+                        print("my name is", MY_NAME)
 
-				else:
-					network_parser(incoming)
+                    else:
+                        network_parser(incoming)
 
-	except Exception as e:
-		s.close()
-		print(e)
+    except Exception as e:
+        s.close()
+        print(e)
 
 def network_parser(text_to_parse):
 	
-	start_of_message = text_to_parse.find(":") # "('127.12.34.56', 34278): hello world!!
+    start_of_message = text_to_parse.find(":") # "('127.12.34.56', 34278): hello world!!
 
-	try:
-		assert start_of_message != -1  # Invalid
+    try:
+        assert start_of_message != -1  # Invalid
 
-		if MY_NAME == None:
-			source = text_to_parse[:start_of_message]  # "('127.12.34.56', 34278)"
-			assert source[0] == "(" and source[-1] == ")"
+        if MY_NAME == None:
+            source = text_to_parse[:start_of_message]  # "('127.12.34.56', 34278)"
+            assert source[0] == "(" and source[-1] == ")"
 
-			source = source[1:-1]  # "'127.12.34.56', 34278"
+            source = source[1:-1]  # "'127.12.34.56', 34278"
 
-			conn = source.split(", ")  # ["'127.12.34.56'", "34278"]
-			conn_ip = conn[0].replace("'","").replace('"','')  # "127.12.34.56"
-			conn_port = int(conn[1])  # 24278
-			conn = (conn_ip, conn_port) # cleaned up
+            conn = source.split(", ")  # ["'127.12.34.56'", "34278"]
+            conn_ip = conn[0].replace("'","").replace('"','')  # "127.12.34.56"
+            conn_port = int(conn[1])  # 24278
+            conn = (conn_ip, conn_port) # cleaned up
 
-			message = text_to_parse[start_of_message+2:]  # ignoring the space
+            message = text_to_parse[start_of_message+2:]  # ignoring the space
 
-			tuplified_msg = tuple(message.split(" "))  # oh god this can't be a good idea
+            tuplified_msg = tuple(message.split(" "))  # oh god this can't be a good idea
 
-		else:
-			message = text_to_parse[start_of_message+2:]
-			tuplified_msg = tuple(message.split(" "))
+        else:
+            message = text_to_parse[start_of_message+2:]
+            tuplified_msg = tuple(message.split(" "))
 
-		if tuplified_msg[0] == MY_NAME:
-			try:
-				assert len(tuplified_msg) >= 2
-			except AssertionError:
-				return
-			text_handler.do_stuff(" ".join(tuplified_msg[1:]))
+            if tuplified_msg[0] == MY_NAME:
+                try:
+                    assert len(tuplified_msg) >= 2
+                except AssertionError:
+                    return
+                text_handler.do_stuff(" ".join(tuplified_msg[1:]))
 
-		elif tuplified_msg[0] == "tell":
-			try:
-				assert len(tuplified_msg) >= 3
-			except AssertionError:
-				return
-			if tuplified_msg[1] == MY_NAME:
-				text_handler.do_stuff(" ".join(tuplified_msg[2:]))
+            elif tuplified_msg[0] == "tell":
+                try:
+                    assert len(tuplified_msg) >= 3
+                except AssertionError:
+                    return
+            if tuplified_msg[1] == MY_NAME:
+                text_handler.do_stuff(" ".join(tuplified_msg[2:]))
 
 
-	except AssertionError:
-		return
+    except AssertionError:
+        return
 
 def go_online():
-	global mouth
-	global ears
+    global mouth
+    global ears
 
-	mouth = threading.Thread(target=sender)
-	ears = threading.Thread(target=listener)
+    mouth = threading.Thread(target=sender)
+    ears = threading.Thread(target=listener)
 
-	mouth.start()
-	ears.start()
+    mouth.start()
+    ears.start()
 
 if __name__ == '__main__':
-	go_online()
+    go_online()
--- server_communication/chat_socket.py
+++ server_communication/chat_socket.py
@@ -5,6 +5,7 @@ Used by the server.
 import socket as sock
 import threading
 
+import encryption
 
 LOCAL_IP = "192.168.1.234"
 LOCAL_PORT = 13131
@@ -21,116 +22,124 @@ MAX_ATTEMPTS = 3
 ATTEMPTS = 0
 
 def set_name(conn, conn_ip, name=None):
-	"""
-	Similar to a DNS service, pretty much.
-	"""
-	print("found new client:", conn_ip)
-	if name == None:	
-		global NAMES
-		assigning_name = NAMES.pop(0)
-	else:
-		assigning_name = name
-	name_set = "set_name "+assigning_name
-	conn.sendall(name_set.encode("ascii"))
+        """
+        Similar to a DNS service, pretty much.
+        """
+        print("found new client:", conn_ip)
+        if name == None:        
+                global NAMES
+                assigning_name = NAMES.pop(0)
+        else:
+                assigning_name = name
+        name_set = "set_name "+assigning_name
+        conn.sendall(name_set.encode("ascii"))
 
-	global current_connections
-	current_connections_ = current_connections.copy()  # python is a baby who is afraid of things changing size
-	for pre_names, pre_conns in current_connections_.items():
-		if pre_conns == [conn_ip, conn]:
-			del current_connections[pre_names]
-		if pre_names == name:
-			return False  # name is in use already! this causes changing to self's old name not being allowed but welp
+        global current_connections
+        current_connections_ = current_connections.copy()  # python is a baby who is afraid of things changing size
+        for pre_names, pre_conns in current_connections_.items():
+                if pre_conns == [conn_ip, conn]:
+                        del current_connections[pre_names]
+                if pre_names == name:
+                        return False  # name is in use already! this causes changing to self's old name not being allowed but welp
 
-	current_connections[assigning_name] = [conn_ip, conn]
+        current_connections[assigning_name] = [conn_ip, conn]
 
-	return assigning_name
+        return assigning_name
 
 
 def close_session(conn, conn_ip):
-	"""
-	Actually not functional, per default behaviour, as clients already close their session from their side.
-	"""
-	try:
-		conn.close()
-		current_connections.pop(conn_ip)
-		print("<== Closed session {} ==>".format(conn_ip))
-	except Exception as e:
-		print("<== Couldn't close session on {} ==>: {}".format(conn_ip, e))
-		return False
-	finally:
-		return True
+        """
+        Actually not functional, per default behaviour, as clients already close their session from their side.
+        """
+        try:
+            conn.close()
+            current_connections.pop(conn_ip)
+            print("<== Closed session {} ==>".format(conn_ip))
+        except Exception as e:
+            print("<== Couldn't close session on {} ==>: {}".format(conn_ip, e))
+            return False
+        finally:
+            return True
 
 def broadcast(msg, src_ip, src_conn=None, exclude_self=True):  # src_conn required if sending in return too 
-	global current_connections
-	others = [client[1] for client in current_connections.values() if client[0] != src_ip]  # client = [conn, src_ip]
-	
-	print(current_connections)
-	for src_name, location in current_connections.items():
-		if location[0] == src_ip:
-			whomstve = src_name
-	stream_msg = str(whomstve) + ": " + msg.decode("ascii")
+    global current_connections
+    others = [client[1] for client in current_connections.values() if client[0] != src_ip]  # client = [conn, src_ip]
+    
+    print(current_connections)
+    for src_name, location in current_connections.items():
+        if location[0] == src_ip:
+            whomstve = src_name
+    stream_msg = str(whomstve) + ": " + msg.decode("ascii")
 
-	for client in others:
-		client.sendall(stream_msg.encode("ascii"))
-	if not exclude_self:
-		src_conn.sendall(stream_msg.encode("ascii"))
+    for client in others:
+        client.sendall(stream_msg.encode("ascii"))
+    if not exclude_self:
+        src_conn.sendall(stream_msg.encode("ascii"))
 
 
 def new_connection(conn, conn_ip):
-	host_name = set_name(conn, conn_ip)  # also adds to connection host list
-	if host_name:
-		print("set", host_name, "to", conn)
-	else:
-		print("couldnt set name")
+    encryption_key = None
+    host_name = set_name(conn, conn_ip)  # also adds to connection host list
+    if host_name:
+        print("set", host_name, "to", conn)
+    else:
+        print("couldnt set name")
 
-	try:
-		while True:
-			try:
-				income = b""
-				income = conn.recv(1024)
-				print("{}: {}".format(conn_ip, income))
+    try:
+        while True:
+            try:
+                income = b""
+                income = conn.recv(1024)
+                print("{}: {}".format(conn_ip, income))
 
-				if "set name" in income.decode("ascii"):
-					host_name = set_name(conn, conn_ip, name=income.decode("ascii")[income.decode("ascii").find("my name is")+len("my name is"):])
-					if host_name:
-						print("set", host_name, "to", conn)
-					else:
-						print("couldn't set name")
+                if "set name" in income.decode("ascii"):
+                    host_name = set_name(conn, conn_ip, name=income.decode("ascii")[income.decode("ascii").find("my name is")+len("my name is"):])
+                    if host_name:
+                        print("set", host_name, "to", conn)
+                    else:
+                        print("couldn't set name")
 
-				elif income not in [b"q", b"end", b"bye", b""]:
-					broadcast(income, conn_ip)
+                if "encrypt" in income.decode("ascii"):
+                    pub_key, priv_key = encryption.public_encryption()
+                    give_key = "public_key "+str(priv_key)
+                    conn.sendall(give_key.enconde("ascii"))
 
-				else:
-					print("{}: <end message>".format(conn_ip))
-					close_session(conn, conn_ip)
-					return
+                elif income not in [b"q", b"end", b"bye", b""]:
+                    decrypted = encryption.decrypt_string(income.decode("ascii"), priv_key)
+                
+                    broadcast(income, conn_ip)
 
-			except Exception as e:
-				print(e)
-				close_session(conn, conn_ip)
-				return
+                else:
+                    print("{}: <end message>".format(conn_ip))
+                    close_session(conn, conn_ip)
+                    return
 
-	except Exception as e:
-		print(e)
-		close_session(conn, conn_ip)
+            except Exception as e:
+                print(e)
+                close_session(conn, conn_ip)
+                return
+
+    except Exception as e:
+        print(e)
+        close_session(conn, conn_ip)
 
 threads = list()
 try:
-	while ATTEMPTS < MAX_ATTEMPTS:
-		conn, conn_ip = s.accept()
+    while ATTEMPTS < MAX_ATTEMPTS:
+        conn, conn_ip = s.accept()
 
-		try:
-			conn_thread = threading.Thread(target=new_connection, args=(conn, conn_ip))
-			# threads.append(conn_thread)
-			conn_thread.start()
-		except Exception as e:
-			print(e)
-			s.close()
-		finally:
-			ATTEMPTS += 1
+        try:
+            conn_thread = threading.Thread(target=new_connection, args=(conn, conn_ip))
+            # threads.append(conn_thread)
+            conn_thread.start()
+        except Exception as e:
+            print(e)
+            s.close()
+        finally:
+            ATTEMPTS += 1
 except:
-	for connection in current_connections:
-		close_session(connection)
+        for connection in current_connections:
+            close_session(connection)
 finally:
-	for connection in current_connections:
-		close_session(connection)
+        for connection in current_connections:
+            close_session(connection)
